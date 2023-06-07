9848ce08698b51d70a7cccfcfe1af9e02703a099
korayakar <korayakar111@gmail.com>
2023-06-08 01:42:24 +0300

    Player Health and Level System

---
+++ GameProject/Assets/My Scripts/LevelSystem.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelSystem : MonoBehaviour
+{
+    public int level;
+    public float currentXp;
+    public float requiredXp;
+
+    private float lerpTimer;
+    private float delayTimer;
+
+    [Header("UI")]
+    public Image frontXpBar;
+    public Image backXpBar;
+    public TextMeshProUGUI levelText;
+    public TextMeshProUGUI xpText;
+
+    [Header("Multipliers")]
+    [Range(1f, 300f)]
+    public float additionMultiplier = 300;
+    [Range(2f, 4f)]
+    public float powerMultiplier = 2;
+    [Range(7f, 14f)]
+    public float divisionMultiplier = 7;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        frontXpBar.fillAmount = currentXp / requiredXp;
+        backXpBar.fillAmount = currentXp / requiredXp;
+        requiredXp = CalculateRequiredXp();
+        levelText.text = "Level " + level;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateXpUI();
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            GainExperienceFlatRate(20);
+        }
+
+        if(currentXp > requiredXp)
+        {
+            LevelUp();
+        }
+    }
+    public void UpdateXpUI()
+    {
+        float xpFraction = currentXp / requiredXp;
+        float FXP = frontXpBar.fillAmount;
+        if(FXP < xpFraction)
+        {
+            delayTimer += Time.deltaTime;
+            backXpBar.fillAmount = xpFraction;
+            if(delayTimer > 3)
+            {
+                lerpTimer += Time.deltaTime;
+                float percentComplete = lerpTimer / 4;
+                frontXpBar.fillAmount = Mathf.Lerp(FXP, backXpBar.fillAmount, percentComplete);
+            }
+        }
+        xpText.text = currentXp + "/" + requiredXp;
+    }
+
+    public void GainExperienceFlatRate(float xpGained)
+    {
+        currentXp += xpGained;
+        lerpTimer = 0f;
+        delayTimer = 0f;
+    }
+    public void GainExperienceScalable(float xpGained, int passedLevel)
+    {
+        if(passedLevel < level)
+        {
+            float multiplier = 1 + (level - passedLevel) * 0.1f;
+            currentXp += xpGained * multiplier;
+        }
+        else
+        {
+            currentXp += xpGained;
+        }
+        lerpTimer = 0f;
+        delayTimer = 0f;
+    }
+    public void LevelUp()
+    {
+        level++;
+        frontXpBar.fillAmount = 0f;
+        backXpBar.fillAmount = 0f;
+        currentXp = Mathf.RoundToInt(currentXp - requiredXp);
+        GetComponent<PlayerHealth>().IncreaseHealth(level);
+        requiredXp = CalculateRequiredXp();
+        levelText.text = "Level " + level;
+    }
+
+    private int CalculateRequiredXp() //Reference = https://oldschool.runescape.wiki/w/Experience
+    {
+        int solveForRequiredXp = 0;
+        for(int levelCycle = 1; levelCycle <= level; levelCycle++)
+        {
+            solveForRequiredXp += (int)Mathf.Floor(levelCycle + additionMultiplier * Mathf.Pow(powerMultiplier, levelCycle / divisionMultiplier));
+        }
+        return solveForRequiredXp / 4;
+    }
+}
+
---
+++ GameProject/Assets/My Scripts/PlayerHealth.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private float health;
+    private float lerpTimer;
+    public float maxHealth = 100f;
+    public float chipSpeed = 2f;
+
+    [Header("UI")]
+    public Image frontHealthBar;
+    public Image backHealthBar;
+    public TextMeshProUGUI healthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = Mathf.Clamp(health, 0, maxHealth);
+        UpdateHealthUI();
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            TakeDamage(Random.Range(5, 10));
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            RestoreHealth(Random.Range(5, 10));
+        }
+    }
+    public void UpdateHealthUI()
+    {
+        Debug.Log(health);
+        float fillFront = frontHealthBar.fillAmount;
+        float fillBack = backHealthBar.fillAmount;
+        float hFraction = health / maxHealth; // to be between 0 and 1
+
+        if (fillBack > hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            backHealthBar.fillAmount = Mathf.Lerp(fillBack, hFraction, percentComplete);
+        }
+
+        if (fillFront < hFraction)
+        {
+            backHealthBar.color = Color.green;
+            backHealthBar.fillAmount = hFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillFront, backHealthBar.fillAmount, percentComplete);
+        }
+        healthText.text = Mathf.Round(health) + "/" + Mathf.Round(maxHealth);
+    }
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        lerpTimer = 0f;
+    }
+    public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
+        lerpTimer = 0f;
+    }
+
+    public void IncreaseHealth(int level)
+    {
+        maxHealth += (health * 0.01f) * ((100 - level) * 0.1f);
+        health = maxHealth;
+    }
+}
