8aa73c152b2027fb0b7677db255c8a5ae0b1fd3a
korayakar <korayakar111@gmail.com>
2023-06-21 18:07:07 +0300

    Added Gun to the Player

---
+++ GameProject/Assets/My Scripts/Gun.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
+
+    public int maxAmmo = 30;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
+    public Camera fpsCamera;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    public Animator animator;
+
+    private float nextTimeToFire = 0f;
+
+
+    private void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+    void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
+
+    void Update()
+    {
+        if (isReloading)
+        {
+            return;
+        }
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+
+    }
+    IEnumerator Reload()
+    {
+        isReloading = true;
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(1f);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        currentAmmo--;
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            UnityEngine.Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 0.5f);
+        }
+
+    }
+}
--- GameProject/Assets/My Scripts/LevelSystem.cs
+++ GameProject/Assets/My Scripts/LevelSystem.cs
@@ -111,3 +111,7 @@ public class LevelSystem : MonoBehaviour
     }
 }
 
+
+
+
+
